38741be1a2fd6ad945d83c7d578f881b242aa8e7
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2019-01-28 14:49:43 -0500

    Implement 'Add System'

--- src/SmartComponents/DriftTable/DriftTable.js
+++ src/SmartComponents/DriftTable/DriftTable.js
@@ -3,18 +3,33 @@ import { withRouter } from 'react-router-dom';
 import { get } from 'axios';
 import { DRIFT_API_ROOT } from '../../constants';
 import './drift-table.scss';
-
 import { Section, Main, PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
-import { Card, CardBody } from '@patternfly/react-core';
-
-import { Button } from '@patternfly/react-core';
+import { Button, Card, CardBody } from '@patternfly/react-core';
+import { AddSystem } from './AddSystem';
 
 class DriftTable extends Component {
     constructor(props) {
         super(props);
-        this.state = { response: '' };
+        this.state = {
+            response: '',
+            modalResponse: ''
+        };
         this.getDriftResponse = this.getDriftResponse.bind(this);
+        this.addSystemModal = this.addSystemModal.bind(this);
         this.getDriftResponse();
+    }
+
+    addSystemModal() {
+        return window.insights.chrome.auth.getUser()
+        .then(() => {
+            get(DRIFT_API_ROOT.concat('/status'))
+            .then(
+                (result) => {
+                    this.setState({
+                        modalResponse: result.data.status
+                    });
+                });
+        });
     }
 
     getDriftResponse() {
@@ -33,7 +48,7 @@ class DriftTable extends Component {
         });
     }
 
-    renderRow(data) {
+    renderRow(data, addSystemData) {
         if (data.facts === undefined) {
             return [];
         }
@@ -45,6 +60,7 @@ class DriftTable extends Component {
                 <tr>
                     <td>{ data.facts[i].name }</td>
                     <td>{ data.facts[i].status }</td>
+                    <td>{ addSystemData }</td>
                 </tr>
             );
         }
@@ -52,9 +68,28 @@ class DriftTable extends Component {
         return rows;
     }
 
-    /*eslint-enable*/
+    renderHeaderRow(data) {
+        if (data === undefined) {
+            return [];
+        }
+
+        let row = [];
+        let hostKeys = data[0].hosts.map(function(host) {
+            return Object.keys(host);
+        });
+        let hostnames = hostKeys.flat();
+
+        for (let i = 0; i < hostnames.length; i++) {
+            row.push(
+                <th>{ hostnames[i] }</th>
+            );
+        }
+
+        return row;
+    }
+
     render() {
-        const { response } = this.state;
+        const { response, modalResponse } = this.state;
 
         return (
             <React.Fragment>
@@ -79,10 +114,15 @@ class DriftTable extends Component {
                                         <tr>
                                             <th>Name</th>
                                             <th>State</th>
+                                            { this.renderHeaderRow(response.facts) }
+                                            <th>
+                                                <AddSystem
+                                                    getAddSystemModal={ this.addSystemModal } />
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        { this.renderRow(response) }
+                                        { this.renderRow(response, modalResponse) }
                                     </tbody>
                                 </table>
                             </div>
