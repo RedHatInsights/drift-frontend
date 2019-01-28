8902caa47b2be00cf126da2248c805bd6cada9fc
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2019-01-28 14:49:43 -0500

    Implement 'Add System'

---
+++ src/SmartComponents/DriftTable/AddSystem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@patternfly/react-core';
+
+export class AddSystem extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Button
+                variant='primary'
+                onClick={ this.props.getAddSystemModal }>
+                Add System
+            </Button>
+        );
+    }
+}
+
+AddSystem.propTypes = {
+    getAddSystemModal: PropTypes.function
+};
