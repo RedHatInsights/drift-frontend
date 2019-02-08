7a6013d76c4d702ac25b7e5a9b1df60262e44d61
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2019-02-08 14:00:29 -0500

    Tests for compare reducers
    Multiple tests written for improved coverage.

---
+++ src/SmartComponents/modules/__tests__/reducer-test.js
@@ -0,0 +1,118 @@
+import reducers from '../reducers';
+import types from '../types';
+
+import { compareReducerPayload, compareReducerState } from './reducer.fixtures';
+
+describe('compare reducer', () => {
+    it('should return initial state', () => {
+        expect(reducers.compareReducer(undefined, {})).toEqual(
+            {
+                compare: {},
+                addSystemModalOpened: false,
+                factFilter: '',
+                filterDropdownOpened: false,
+                selectedSystemIds: [],
+                stateFilter: 'all'
+            }
+        );
+    });
+
+    it('should handle FETCH_COMPARE_FULFILLED', () => {
+        expect(
+            reducers.compareReducer({}, {
+                payload: compareReducerPayload,
+                type: `${types.FETCH_COMPARE}_FULFILLED`
+            })
+        ).toEqual({
+            compare: compareReducerState,
+            selectedSystemIds: [
+                '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9',
+                'f35b1e1d-d231-43f2-8e4f-8f9cb01e3aa2'
+            ]
+        });
+    });
+
+    it('should handle SELECT_ENTITY', () => {
+        expect(
+            reducers.compareReducer({ selectedSystemIds: []}, {
+                payload:
+                    {
+                        id: '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9', selected: true
+                    },
+                type: `${types.SELECT_ENTITY}`
+            })
+        ).toEqual({
+            selectedSystemIds: [
+                '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9'
+            ]
+        });
+    });
+
+    it('should handle multiple SELECT_ENTITY', () => {
+        expect(
+            reducers.compareReducer({ selectedSystemIds: [ '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9' ]}, {
+                payload:
+                    {
+                        id: 'f35b1e1d-d231-43f2-8e4f-8f9cb01e3aa2', selected: true
+                    },
+                type: `${types.SELECT_ENTITY}`
+            })
+        ).toEqual({
+            selectedSystemIds: [
+                '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9',
+                'f35b1e1d-d231-43f2-8e4f-8f9cb01e3aa2'
+            ]
+        });
+    });
+
+    it('should handle false SELECT_ENTITY', () => {
+        expect(
+            reducers.compareReducer({ selectedSystemIds: [ '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9' ]}, {
+                payload:
+                    {
+                        id: 'f35b1e1d-d231-43f2-8e4f-8f9cb01e3aa2', selected: false
+                    },
+                type: `${types.SELECT_ENTITY}`
+            })
+        ).toEqual({
+            selectedSystemIds: [
+                '9c79efcc-8f9a-47c7-b0f2-142ff52e89e9'
+            ]
+        });
+    });
+});
+
+describe('add system modal reducer', () => {
+    it('should return initial state', () => {
+        expect(reducers.addSystemModalReducer(undefined, {})).toEqual(
+            {
+                compare: {},
+                addSystemModalOpened: false,
+                factFilter: '',
+                filterDropdownOpened: false,
+                selectedSystemIds: [],
+                stateFilter: 'all'
+            }
+        );
+    });
+
+    it('should handle OPEN_ADD_SYSTEM_MODAL true', () => {
+        expect(
+            reducers.addSystemModalReducer({ addSystemModalOpened: false }, {
+                type: `${types.OPEN_ADD_SYSTEM_MODAL}`
+            })
+        ).toEqual({
+            addSystemModalOpened: true
+        });
+    });
+
+    it('should handle OPEN_ADD_SYSTEM_MODAL false', () => {
+        expect(
+            reducers.addSystemModalReducer({ addSystemModalOpened: true }, {
+                type: `${types.OPEN_ADD_SYSTEM_MODAL}`
+            })
+        ).toEqual({
+            addSystemModalOpened: false
+        });
+    });
+});
