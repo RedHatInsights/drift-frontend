4a6cf8ba38e57de9ccc8607c06cf8c3e27c5159d
Ryan Long <rlong@redhat.com>
2019-02-18 10:17:41 -0500

    identify app as drift

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
     componentDidMount () {
         insights.chrome.init();
-        insights.chrome.identifyApp('inventory');
+        insights.chrome.identifyApp('drift');
     }
 
     componentWillUnmount () {
