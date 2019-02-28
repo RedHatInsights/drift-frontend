6f385787200f4a6e1f343100e9c01e53cf271051
Eric D. Helms <ericdhelms@gmail.com>
2019-02-28 16:57:22 -0500

    Update host styling to add background and last updated

--- src/SmartComponents/DriftPage/DriftTable/AddSystem.js
+++ src/SmartComponents/DriftPage/DriftTable/AddSystem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@patternfly/react-core';
+import { AddCircleOIcon } from '@patternfly/react-icons';
 
 export class AddSystem extends Component {
     constructor(props) {
@@ -9,11 +10,16 @@ export class AddSystem extends Component {
 
     render() {
         return (
-            <Button
-                variant='primary'
-                onClick={ this.props.getAddSystemModal }>
-                Add System
-            </Button>
+            <div className="add-system-header">
+                <div className="add-system-icon">
+                    <AddCircleOIcon/>
+                </div>
+                <Button
+                    variant='primary'
+                    onClick={ this.props.getAddSystemModal }>
+                    Add System
+                </Button>
+            </div>
         );
     }
 }
