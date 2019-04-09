42dee9520f43c7c0e1adddf93163fbd008f407e2
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2019-04-09 10:53:53 -0400

    Update api string to V1

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import { version } from './../package.json';
-export const DRIFT_API_ROOT = '/api/drift/v0';
+export const DRIFT_API_ROOT = '/api/drift/v1';
 
 export const API_HEADERS = {
     'X-Insights-Drift': version,
