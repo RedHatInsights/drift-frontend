6c3e847aa4958ab48502fd31bbf3dcee96b3e8fb
Eric D. Helms <ericdhelms@gmail.com>
2019-04-16 16:22:58 -0400

    Push to -stable and -beta branches based on qa master or prod

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -3,16 +3,40 @@ set -e
 set -x
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    echo
+    echo
+    echo "PUSHING ci-beta"
     .travis/release.sh "ci-beta"
-    cd dist
-    git fetch origin ci-stable
-    git push origin ci-beta:ci-stable
+    rm -rf dist/.git
+
+    echo
+    echo
+    echo "PUSHING ci-stable"
+    .travis/release.sh "ci-stable"
 fi
 
-if [[ "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" ]]; then
-    .travis/release.sh "${TRAVIS_BRANCH}"
+if [[ "${TRAVIS_BRANCH}" = "qa" ]]; then
+    echo
+    echo
+    echo "PUSHING qa-beta"
+    .travis/release.sh "qa-beta"
+    rm -rf dist/.git
+
+    echo
+    echo
+    echo "PUSHING qa-stable"
+    .travis/release.sh "qa-stable"
 fi
 
-if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
-    .travis/release.sh "${TRAVIS_BRANCH}"
+if [[ "${TRAVIS_BRANCH}" = "prod" ]]; then
+    echo
+    echo
+    echo "PUSHING prod-beta"
+    .travis/release.sh "prod-beta"
+    rm -rf dist/.git
+
+    echo
+    echo
+    echo "PUSHING prod-stable"
+    .travis/release.sh "prod-stable"
 fi
