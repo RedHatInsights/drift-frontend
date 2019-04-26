9788dc7b3863ca9a5f2d88dc77a7fd5fde043ede
Eric D. Helms <ericdhelms@gmail.com>
2019-04-26 12:56:29 -0400

    Fix select persisting through searches in add systems

--- src/SmartComponents/SystemsTable/reducers.js
+++ src/SmartComponents/SystemsTable/reducers.js
@@ -5,7 +5,7 @@ function selectedReducer(INVENTORY_ACTIONS, selectedSystemIds) {
     return applyReducerHash({
         [INVENTORY_ACTIONS.LOAD_ENTITIES_FULFILLED]: (state, action) => {
             for (let i = 0; i < action.payload.results.length; i += 1) {
-                if (selectedSystemIds.includes(action.payload.results[i].id)) {
+                if (selectedSystemIds().includes(action.payload.results[i].id)) {
                     action.payload.results[i].selected = true;
                 }
             }
