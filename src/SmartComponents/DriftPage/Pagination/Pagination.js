6552794bc1c0ca558de11136769b4bc0add2af52
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2019-06-11 11:25:49 -0400

    Set default items per page to 50

--- src/SmartComponents/DriftPage/Pagination/Pagination.js
+++ src/SmartComponents/DriftPage/Pagination/Pagination.js
@@ -7,7 +7,7 @@ import { Pagination, DropdownDirection } from '@patternfly/react-core';
 
 const perPageOptions = [
     { title: '10', value: 10 },
-    { title: '20', value: 20 },
+    { title: '25', value: 25 },
     { title: '50', value: 50 },
     { title: '100', value: 100 }
 ];
