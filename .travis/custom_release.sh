2c6d39bcb8c11335a5f706c95f4a124f302683ba
Eric D. Helms <ericdhelms@gmail.com>
2019-10-04 10:08:13 -0400

    Fix beta bits landing in ci-stable

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -7,14 +7,15 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo
     echo "PUSHING ci-beta"
     .travis/release.sh "ci-beta"
-    rm -rf dist/.git
+    rm -rf dist/
 
     echo
     echo
     echo "PUSHING ci-stable"
+    BUILD_STABLE=true npm run travis:build
     .travis/release.sh "ci-stable"
 fi
 
-if [[ "${TRAVIS_BRANCH}" = "ci-stable"  || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+if [["${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
