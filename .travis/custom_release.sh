7f939c4601ac1d6a70a23bd7d45226ae00501bca
Eric D. Helms <ericdhelms@gmail.com>
2019-10-07 13:56:17 -0400

    Auto push to QA beta and stable

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -7,15 +7,20 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo
     echo "PUSHING ci-beta"
     .travis/release.sh "ci-beta"
-    rm -rf dist/
+    rm -rf dist/.git
+    .travis/release.sh "qa-beta"
 
+    echo "Rebuilding for stable branches"
+    rm -rf dist/
+    BUILD_STABLE=true npm run travis:build
     echo
     echo
     echo "PUSHING ci-stable"
-    BUILD_STABLE=true npm run travis:build
     .travis/release.sh "ci-stable"
+    rm -rf dist/.git
+    .travis/release.sh "qa-stable"
 fi
 
-if [["${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
