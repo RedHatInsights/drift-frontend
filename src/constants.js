02d1d49682bba55a0982a4bd38504dc550525d3e
Chris Duryee <cduryee@redhat.com>
2019-10-08 15:49:35 -0400

    use v1 system-baseline api

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 import { version } from './../package.json';
 export const DRIFT_API_ROOT = '/api/drift/v1';
-export const BASELINE_API_ROOT = '/api/system-baseline/v0';
+export const BASELINE_API_ROOT = '/api/system-baseline/v1';
 export const ASC = 'asc';
 export const DESC = 'desc';
 export const EXPERIMENTAL_COOKIE_NAME = 'drift-experimental-features';
