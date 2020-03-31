46e5a696fed8ea8872794bcb2e6688db3daf1d28
Eric D. Helms <ericdhelms@gmail.com>
2020-03-31 14:26:47 -0400

    Fix copy from an existing system for baseline creation

--- src/SmartComponents/SystemsTable/reducers.js
+++ src/SmartComponents/SystemsTable/reducers.js
@@ -20,7 +20,7 @@ function selectedReducer(INVENTORY_ACTIONS, createBaselineModal, historicalProfi
 
             let rows = mergeArraysByKey([ action.payload.results, state.rows ]);
 
-            if (insights.chrome.isBeta()) {
+            if (insights.chrome.isBeta() && historicalProfiles !== undefined) {
                 /*eslint-disable camelcase*/
                 if (!newColumns.find(({ key }) => key === 'historical_profiles') && hasHistoricalDropdown) {
                     newColumns.push({
