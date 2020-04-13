391a13290e46e712bfc86184dc0c3118e42e49a1
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2020-04-13 11:40:01 -0400

    Update HSP api version to v1

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 import { version } from './../package.json';
 export const DRIFT_API_ROOT = '/api/drift/v1';
 export const BASELINE_API_ROOT = '/api/system-baseline/v1';
-export const HISTORICAL_PROFILES_API_ROOT = '/api/historical-system-profiles/v0';
+export const HISTORICAL_PROFILES_API_ROOT = '/api/historical-system-profiles/v1';
 export const ASC = 'asc';
 export const DESC = 'desc';
 export const FACT_ID = 0;
