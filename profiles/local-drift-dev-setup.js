571d7726d6e696aa92a6eae2cae3ebc34ddedbb3
Petr Blaho <petrblaho@redhat.com>
2021-01-20 21:15:13 +0100

    Adds config for spandx to run local frontend alongside drift-dev-setup

---
+++ profiles/local-drift-dev-setup.js
@@ -0,0 +1,24 @@
+/*global module*/
+
+const SECTION = 'insights';
+const APP_ID = 'drift';
+const FRONTEND_PORT = 8001;
+const routes = {};
+
+// backend
+routes[`/api/inventory`] = { host: `http://localhost:8082` };
+routes[`/api/system-baseline`] = { host: `http://localhost:8083` };
+routes[`/api/drift`] = { host: `http://localhost:8084` };
+routes[`/api/historical-system-profiles`] = { host: `http://localhost:8085` };
+
+// frontend
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
+
+
+module.exports = { routes };
+
+
+
