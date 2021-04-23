4e2360f3879df22ef28200fe4f123a08701dd320
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2021-04-23 16:17:07 -0400

    Add DevEntry for redux logging
    Dev environment stopped logging redux actions in the browser console. This PR creates a dev-specific entry point into the app where logging is passed, exclusively.
    
    To repro:
    Open app and open developer tools in browser
    You should not see any redux actions in the console
    
    This fix will expose the action logging.

--- src/DevEntry.js
+++ src/DevEntry.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
+import logger from 'redux-logger';
 
 import getBaseName from './Utilities/getBaseName';
 
-const Drift = () => (
-    <Provider store={ init().getStore() }>
+const DriftDev = () => (
+    <Provider store={ init(logger).getStore() }>
         <Router basename={ getBaseName(window.location.pathname) }>
             <App/>
         </Router>
     </Provider>
 );
 
-export default Drift;
+export default DriftDev;
