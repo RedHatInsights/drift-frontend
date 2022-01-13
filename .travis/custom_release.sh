c5e61182fe361672611d52d94e609a9b0be5bcda
Michael Johnson <johnsonm325@gmail.com>
2022-01-13 02:43:37 -0500

    Update custom release script to go master -> qa (#556)
    Before, a merge to master released directly to ci-beta and ci-stable. And a push to qa had to happen seperately.
    
    Now, this PR updates the custom release script so that a merge to master goes to qa-beta and qa-stable. While ci and qa will no longer be used, qa also merges to stage, so this change effectly takes a push to master directly to stage. We will still utilize our prod release script to push from stage to prod.
    
    Co-authored-by: Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -5,32 +5,16 @@ set -x
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo
     echo
-    echo "PUSHING ci-beta"
-    .travis/release.sh "ci-beta"
+    echo "PUSHING qa-beta"
+    .travis/release.sh "qa-beta"
 
     echo "Rebuilding for stable branches"
     rm -rf dist/
     BUILD_STABLE=true npm run travis:build
     echo
     echo
-    echo "PUSHING ci-stable"
-    .travis/release.sh "ci-stable"
-fi
-
-if [ "${TRAVIS_BRANCH}" = "qa" ]; then
-    echo "Building for qa-stable"
-    echo
-    echo
     echo "PUSHING qa-stable"
     .travis/release.sh "qa-stable"
-
-    echo "Rebuilding for qa-beta"
-    rm -rf dist/
-    BUILD_BETA=true npm run travis:build
-    echo
-    echo
-    echo "PUSHING qa-beta"
-    .travis/release.sh "qa-beta"
 fi
 
 if [ "${TRAVIS_BRANCH}" = "prod" ]; then
