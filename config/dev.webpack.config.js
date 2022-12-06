2a9637322153f5e8ad42c140273e48ce1b0007cd
Jiri Dostal <jdostal@redhat.com>
2022-12-06 19:29:38 +0100

    fix(webpack): DRFT-1219 - Drift breaks other apps when loaded first

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -17,7 +17,14 @@ plugins.push(
         root: resolve(__dirname, '../'),
         exposes: {
             './RootApp': resolve(__dirname, '../src/DevEntry')
-        }
+        },
+        exclude: [ 'react-redux' ],
+        shared: [{
+            'react-redux': {
+                requiredVersion: '*',
+                singleton: true
+            }
+        }]
     })
 );
 
