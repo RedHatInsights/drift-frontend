434c00bfa4559b54318e38af7fa82861ff0a4912
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2023-04-06 12:58:35 -0400

    chore(RHIF-219): Update /beta to /preview
    To test, you can run via /beta or /preview. We previously had issues with the local app being served. To ensure that it is properly being served, run the app via beta and make a change in the code or add a console log and ensure that you're getting local changes.

--- profiles/local-frontend-and-ephemeral-cluster.js
+++ profiles/local-frontend-and-ephemeral-cluster.js
@@ -12,13 +12,9 @@ routes[`/api/drift`] = { host: `http://localhost:8084` };
 routes[`/api/historical-system-profiles`] = { host: `http://localhost:8085` };
 
 // frontend
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/preview/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/preview/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
 
-
 module.exports = { routes };
-
-
-
--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -5,9 +5,9 @@ const APP_ID = 'drift';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/preview/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/preview/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
