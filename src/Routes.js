6ee6c1d7e15163837e782f536d71e953909bf4ba
Adonis Puente <adonis.puente@gmail.com>
2023-05-02 12:26:37 -0400

    feat(RHIN-830): Add Zero State to Drift

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,11 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import asyncComponent from './Utilities/asyncComponent';
+import axios from 'axios';
+import AsynComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
+import { ErrorState } from '@redhat-cloud-services/frontend-components';
 
 const DriftPage = asyncComponent(() => import ('./SmartComponents/DriftPage/DriftPage'));
 const BaselinesPage = asyncComponent(() => import ('./SmartComponents/BaselinesPage/BaselinesPage'));
@@ -10,8 +13,30 @@ const EditBaselinePage = asyncComponent(() => import ('./SmartComponents/Baselin
 
 const InsightsRoute = ({ component: Component, title, ...rest }) => {
     title ? document.title = title : null;
+    const INVENTORY_TOTAL_FETCH_URL = '/api/inventory/v1/hosts';
+    const [ hasSystems, setHasSystems ] = useState(false);
+    useEffect(() => {
+        try {
+            axios
+            .get(`${INVENTORY_TOTAL_FETCH_URL}?page=1&per_page=1`)
+            .then(({ data }) => {
+                setHasSystems(data.total > 0);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }, [ hasSystems ]);
 
-    return (<Route { ...rest } component={ Component } />);
+    return (
+        !hasSystems ?
+            <AsynComponent
+                appName="dashboard"
+                module="./AppZeroState"
+                scope="dashboard"
+                ErrorComponent={ <ErrorState /> }
+                app="Drift"
+            />
+            : <Route { ...rest } component={ Component } />);
 };
 
 InsightsRoute.propTypes = {
