e594d95b4b29d15360cb883e1ad4657b3caea5ba
Georgii Karataev <georgkarat@gmail.com>
2024-08-05 14:26:34 +0200

    chore(RHINENG-10786): Move away from /preview (#702)
    This removes obsolete files and profile entries. It has no effect on the behaviour.
    
    https://issues.redhat.com/browse/RHINENG-10786

--- profiles/local-frontend-and-ephemeral-cluster.js
+++ profiles/local-frontend-and-ephemeral-cluster.js
@@ -12,9 +12,7 @@ routes[`/api/drift`] = { host: `http://localhost:8084` };
 routes[`/api/historical-system-profiles`] = { host: `http://localhost:8085` };
 
 // frontend
-routes[`/preview/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/preview/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
